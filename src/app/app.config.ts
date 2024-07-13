66fae581a42df8908fc3dfa959e8fa7ac079e257
Fabio Biondi <info@fabiobiondi.com>
2024-07-13 05:12:22 +0200

    feat: add shop demo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,29 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideEffects } from '@ngrx/effects';
+import * as productsEffects from './core/store/products/products.effects';
+import { productsFeature } from './core/store/products/products.feature';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStore({}, {
-      runtimeChecks: {
-        strictStateSerializability: true,
-        strictActionSerializability: true,
-        strictActionTypeUniqueness: true
-      }
+        runtimeChecks: {
+            strictStateSerializability: true,
+            strictActionSerializability: true,
+            strictActionTypeUniqueness: true
+        }
     }),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
-    provideState({ name: 'home', reducer: () => [1, 2, 3] }),
+    provideState({ name: productsFeature.name, reducer: productsFeature.reducer }),
+    // provideState({ name: 'home', reducer: () => [1, 2, 3] }),
+    provideEffects([productsEffects])
   ]
 };
--- src/app/features/shop/shop.component.ts
+++ src/app/features/shop/shop.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { ProductsActions } from '../../core/store/products/products.actions';
+import { selectList } from '../../core/store/products/products.feature';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [],
   template: `
-    <p>
-      shop works!
-    </p>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+      @for (product of products(); track product.id) {
+        <div class="card bg-slate-900 shadow-xl">
+          <figure>
+            <img [src]="product.image" [alt]="product.name" /></figure>
+          <div class="card-body">
+            <h2 class="card-title">{{product.name}}</h2>
+            <div class="card-actions justify-end">
+              <button class="btn btn-outline btn-primary">Add to Cart  | € {{product.cost}}</button>
+            </div>
+          </div>
+        </div>
+      }
+    </div>
   `,
-  styles: ``
 })
-export default class ShopComponent {
+export default class ShopComponent implements OnInit {
+  store = inject(Store)
+  products = this.store.selectSignal(selectList)
 
+  ngOnInit() {
+    this.store.dispatch(ProductsActions.load())
+  }
 }
